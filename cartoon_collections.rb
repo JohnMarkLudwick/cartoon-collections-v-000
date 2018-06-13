75fb8129da3f031e9bfffabf730fbe7b9c87718f
JohnMark <johnmarkludwick@gmail.com>
2018-06-13 15:25:53 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,8 @@ end
 
 def summon_captain_planet(collection)
   planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-  planeteer_calls.map! {|element| element.capitalize + !}
-  
+  planeteer_calls.map! {|element| element.capitalize}
+  planeteer_calls.each { |element| puts element + '!' } 
   
 end
 
