c2cb9a9a5cf0eb3b00d11d84368472403ef0382d
JohnMark <johnmarkludwick@gmail.com>
2018-06-13 16:46:01 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,28 +9,28 @@ def roll_call_dwarves(array)
   
 end
 
-# def summon_captain_planet(array)
-#   nu_array = []
-#   i = 0 
-#   while i < array.length 
-#     nu_array << array[i].capitalize + "!"
-#     i += 1 
-#   end 
-#   nu_array
-# end
-
-
-
-
 def summon_captain_planet(array)
   nu_array = []
-  i = 0
-  while i < array.length
+  i = 0 
+  while i < array.length 
     nu_array << array[i].capitalize + "!"
-    i += 1
-  end
+    i += 1 
+  end 
   nu_array
 end
+
+
+
+
+# def summon_captain_planet(array)
+#   nu_array = []
+#   i = 0
+#   while i < array.length
+#     nu_array << array[i].capitalize + "!"
+#     i += 1
+#   end
+#   nu_array
+# end
   
 # planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 #   planeteer_calls.map! {|element| element.capitalize}
