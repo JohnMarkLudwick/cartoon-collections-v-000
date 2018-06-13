1c603550bd85a87a6a434e8360c42cec3e3cb993
JohnMark <johnmarkludwick@gmail.com>
2018-06-13 15:24:14 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ end
 
 def summon_captain_planet(collection)
   planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-  planeteer_calls.map! {|element| 
+  planeteer_calls.map! {|element| element.
   
   
 end
